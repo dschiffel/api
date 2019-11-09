85bcf8ec4ed631418bc267d1d97a8b6b89bbdfd1
krlove <mischenkoandrey@gmail.com>
2019-11-09 19:54:51 +0100

    Start states action

--- src/Controller/StateController.php
+++ src/Controller/StateController.php
@@ -2,19 +2,28 @@
 
 namespace App\Controller;
 
+use App\DTO\Assembler\AttributeAssembler;
+use App\DTO\Assembler\EnvironmentAssembler;
+use App\Entity\Environment;
+use App\Entity\State;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\View\View;
 
 class StateController extends AbstractFOSRestController
 {
     /**
      * @Rest\Get("/states/")
-     *
-     * @return View
      */
-    public function getStatesAction()
-    {
-        return $this->view([]);
+    public function getStatesAction(
+        EntityManagerInterface $em,
+        EnvironmentAssembler $environmentAssembler,
+        AttributeAssembler $attributeAssembler
+    ) {
+        $environments = $em->getRepository(Environment::class)->findAll();
+        $attributes = $em->getRepository(Environment::class)->findAll();
+        $states = $em->getRepository(State::class)->findAll();
+
+        // todo continue with changing StateDTO and StateAssembler
     }
 }
