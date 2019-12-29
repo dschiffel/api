b9c38d9f2ec793c79d51a8c47be7bd0db30f025d
krlove <mischenkoandrey@gmail.com>
2019-12-29 16:15:39 +0100

    Implement unique email validator

--- src/Validation/Constraints/UniqueUserEmailConstraintValidator.php
+++ src/Validation/Constraints/UniqueUserEmailConstraintValidator.php
@@ -2,13 +2,28 @@
 
 namespace App\Validation\Constraints;
 
+use App\Repository\UserRepository;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class UniqueUserEmailConstraintValidator extends ConstraintValidator
 {
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function validate($value, Constraint $constraint)
     {
-        // todo validate
+        $exists = $this->userRepository->count(['email' => $value]);
+
+        if ($exists > 0) {
+            $this->context->addViolation(sprintf('Email % is already registered', $value));
+        }
     }
 }
