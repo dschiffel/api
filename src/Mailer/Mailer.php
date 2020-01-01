b37b223feb64389eb39e1dfe2bcdf63474f5fa57
krlove <mischenkoandrey@gmail.com>
2020-01-01 19:37:31 +0100

    Configure mail sending

--- src/Mailer/Mailer.php
+++ src/Mailer/Mailer.php
@@ -3,11 +3,30 @@
 namespace App\Mailer;
 
 use App\Entity\ActionToken;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class Mailer
 {
+    /**
+     * @var MailerInterface
+     */
+    private $symfonyMailer;
+
+    public function __construct(MailerInterface $symfonyMailer)
+    {
+        $this->symfonyMailer = $symfonyMailer;
+    }
+
     public function sendConfirmationEmail(ActionToken $actionToken)
     {
-        
+        $email = new Email();
+        $email
+            ->from('noreply@envstate.com')
+            ->to($actionToken->getUser()->getEmail())
+            ->subject('Confirm your email address')
+            ->html('<p>Please, confirm your email address</p>');
+
+        $this->symfonyMailer->send($email);
     }
 }
